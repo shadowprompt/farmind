f4b8b8d121e9b72e0bb208cbd48e55bed2e6eb72
shadowprompt <huixiong.cn@gmail.com>
2020-10-25 11:17:34 +0800

    update eslint and prettierrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,19 @@
 module.exports = {
   root: true,
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
+    sourceType: 'module'
   },
   env: {
     browser: true,
+    jquery: true
   },
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
+    'plugin:vue/essential',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard'
+    'standard',
   ],
   // required to lint *.vue files
   plugins: [
@@ -21,9 +23,16 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
-    // allow async-await
-    'generator-star-spacing': 'off',
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  }
+    //全等规则关闭
+    'eqeqeq':'off',
+    "no-restricted-globals": 0,
+    "vue/no-parsing-error": [2, { "x-invalid-end-tag": false }],
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'space-before-function-parentheses': 0,
+    'space-before-function-paren': 0,
+    'comma-dangle': 0,
+    'semi': 0,
+  },
+  // 把vue的缩进检测关掉
+  overrides: [ { 'files': ['*.vue'], 'rules': { 'indent': 'off' } } ]
 }
